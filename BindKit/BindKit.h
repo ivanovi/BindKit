d0b6a63d3a8ba7fc5da05c95347bd5cd856f8c9b
Ivan Ivanov <ivanov@creatub.com>
2014-08-27 10:45:14 +0300

    Initial commit

---
+++ BindKit/BindKit.h
@@ -0,0 +1,19 @@
+//
+//  BindKit.h
+//  BindKit
+//
+//  Created by Ivan Ivanov on 8/27/14.
+//  Copyright (c) 2014 Creatub Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BindKit.
+FOUNDATION_EXPORT double BindKitVersionNumber;
+
+//! Project version string for BindKit.
+FOUNDATION_EXPORT const unsigned char BindKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BindKit/PublicHeader.h>
+
+
